2887bd0597e21204c8e617f96d6893829e3b2055
Preet Patel <preet1p3p@gmail.com>
2026-08-05 19:11:27 +0530

    Implement QuickSort algorithm in Java

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -1,5 +1,33 @@
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] args) {
-
+        int[] arr={5,-1,0,-10,8,8,3,-5,2};
+        sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr,int s,int e){
+        if(s>=e){
+            return;
+        }
+        int pIdx=partition(arr,s,e);
+        sort(arr,s,pIdx-1);
+        sort(arr,pIdx+1,e);
+    }
+    static int partition(int[] arr,int s,int e){
+        int pivot=arr[e];
+        int i=s-1;
+        for(int j=s;j<e;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int t=arr[j];
+                arr[j]=arr[i];
+                arr[i]=t;
+            }
+        }
+        i++;
+        arr[e]=arr[i];
+        arr[i]=pivot;
+        return i;
     }
 }
