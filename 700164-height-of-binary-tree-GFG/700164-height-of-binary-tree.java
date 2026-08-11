aeecfdfda2e5a1d7ea5fa91a24b3b7bd198c060c
Preet Patel <preet1p3p@gmail.com>
2026-08-11 23:52:25 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 700164-height-of-binary-tree-GFG/700164-height-of-binary-tree.java
@@ -0,0 +1,26 @@
+/* Structure of Binary Tree Node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}*/
+
+class Solution {
+    public int height(Node root) {
+        // code here
+        if(root==null){
+            return -1;
+        }
+        int l=height(root.left);
+        int r=height(root.right);
+        return Math.max(l,r)+1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
