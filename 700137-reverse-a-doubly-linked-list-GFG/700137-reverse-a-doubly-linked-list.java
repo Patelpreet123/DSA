9b05c0f8beb09804a5eeeb7291d2d56a67863e47
Preet Patel <preet1p3p@gmail.com>
2026-08-23 18:44:29 +0530

    Added solution

---
+++ 700137-reverse-a-doubly-linked-list-GFG/700137-reverse-a-doubly-linked-list.java
@@ -0,0 +1,31 @@
+/* Structure of Doubly Linked List Node
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+*/
+class Solution {
+    public Node reverse(Node head) {
+        // code here
+        Node c=head;
+        Node l=null;
+        while(c!=null){
+            l=c.prev;
+            c.prev=c.next;
+            c.next=l;
+            c=c.prev;
+        }
+        return l.prev;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
