356031707e9ad0a4a62db25fa68c36e01c8f77b1
Preet Patel <preet1p3p@gmail.com>
2026-08-31 11:13:38 +0530

    Time: 10 ms (55.83%), Space: 46.3 MB (50.72%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] x=new int[26];
+        int mx=0;
+        int l=0,r=0;
+        int n=s.length();
+        int a=0;
+        while(r<n){
+            x[s.charAt(r)-'A']++;
+            mx=Math.max(mx,x[s.charAt(r)-'A']);
+            if(r-l+1-mx>k){
+                x[s.charAt(l)-'A']--;
+                l++;
+            }
+            a=Math.max(a,r-l+1);
+            r++;
+        }
+        return a;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
