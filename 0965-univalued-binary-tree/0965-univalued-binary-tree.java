7d57392de5293dddc61391c7eebdc62a0136c57c
Preet Patel <preet1p3p@gmail.com>
2026-09-06 20:19:34 +0530

    Time: 0 ms (100%), Space: 43.1 MB (39.91%) - LeetHub - Time: O(n), Space: O(h)

---
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        return f(root,root.val);
+    }
+    boolean f(TreeNode root,int d){
+        if(root==null){
+            return true;
+        }
+        if(root.val!=d){
+            return false;
+        }
+        return f(root.left,d)&&f(root.right,d);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
