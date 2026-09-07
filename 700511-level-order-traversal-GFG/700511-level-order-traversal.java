23696afd9b4eeb390a434820c46b89cb5cc3372f
Preet Patel <preet1p3p@gmail.com>
2026-09-07 08:20:07 +0530

    Added solution - Time: O(N), Space: O(N)

--- 700511-level-order-traversal-GFG/700511-level-order-traversal.java
+++ 700511-level-order-traversal-GFG/700511-level-order-traversal.java
@@ -18,13 +18,16 @@ class Solution {
         Queue<Node> q=new LinkedList<>();
         q.add(root);
         while(!q.isEmpty()){
-            Node t=q.remove();
-            l.add(t.data);
-            if(t.left!=null){
-                q.add(t.left);
-            }
-            if(t.right!=null){
-                q.add(t.right);
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                Node t=q.remove();
+                l.add(t.data);
+                if(t.left!=null){
+                    q.add(t.left);
+                }
+                if(t.right!=null){
+                    q.add(t.right);
+                }
             }
         }
         return l;
