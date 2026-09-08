2ad8f854f9de27ca287862a85a89a601d2292565
Preet Patel <preet1p3p@gmail.com>
2026-09-08 10:45:02 +0530

    Time: 1 ms (71.13%), Space: 43.6 MB (50.8%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> l=new ArrayList<>();
+        if(root==null){
+            return l;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode t=q.remove();
+                if(i==n-1){
+                    l.add(t.val);
+                }
+                if(t.left!=null){
+                    q.add(t.left);
+                }
+                if(t.right!=null){
+                    q.add(t.right);
+                }
+            }
+        }
+        return l;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
