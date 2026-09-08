5f00943a6969f84fb4918cb0413d8617eaf8faf3
Preet Patel <preet1p3p@gmail.com>
2026-09-08 09:33:34 +0530

    Time: 0 ms (100%), Space: 45 MB (90.84%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        if(root==null){
+            return 0;
+        }
+        int max=0;
+        for(int i=0;i<root.children.size();i++){
+            max=Math.max(max,maxDepth(root.children.get(i)));
+        }
+        return 1+max;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
