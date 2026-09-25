e5e2327e1061a316d8bfa806d780e660d13fd2c7
Preet Patel <preet1p3p@gmail.com>
2026-09-25 09:58:40 +0530

    Added solution - Time: O(n), Space: O(h)

---
+++ 700134-size-of-binary-tree-GFG/700134-size-of-binary-tree.java
@@ -0,0 +1,29 @@
+/*
+Definition for Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Solution {
+    public int getSize(Node root) {
+        // code here
+        if(root==null){
+            return 0;
+        }
+        return 1+getSize(root.left)+getSize(root.right);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
